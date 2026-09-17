8a7a3f767f406d381f3c8a1dc4229252a5b4b8f4
Marcin Kiszka <marcin.kiszka@smseagle.eu>
2026-09-17 12:33:02 +0200

    Added APIv2 implementation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ RCDevs OpenOTP-SMSEAGLE
 
 Plugin for RCDevs OpenOTP SMS tokens with use of SMSEagle device (https://www.smseagle.eu)
 
-Script source: https://bitbucket.org/proximus/smseagle-rcdevs_openotp/
+Script source: https://github.com/smseagle/smseagle-rcdevs_openotp
 
 Published on BSD License
 
--- custom1.php
+++ custom1.php
@@ -2,43 +2,50 @@
 
 /*
  * Custom SMS 1
- * 
+ *
  * Input Variables:
  *  $msg : Text message
  *  $mobile : User mobile number
- *  $flash : true for flash SMS, false for normal SMS 
+ *  $flash : true for flash SMS, false for normal SMS
  *  $sender : Sender mobile number
  *  $username : SMSC connection username
- *  $password : SMSC connection password
+ *  $password : SMSC connection password (used as API token for SMSEagle APIv2)
  *  $account : SMSC account ID
  *  $url : SMSC SOAP service url
  *  $proxy : proxy server connection data
  *  &$error : error message
- * 
+ *
  * Return values:
  *  true for success
  *  false for error
  */
 
 //
-// SMSEagle SMSC connector using HTTP-GET
+// SMSEagle SMSC connector using APIv2 (JSON POST)
 //
 
 function smshub_custom1 ($msg, $mobile, $flash, $sender, $username, $password, $account, $url, $proxy, &$error) {
-    $msg = urlencode($msg);
+    $apiUrl = rtrim($url, '/') . '/api/v2/messages/sms';
 
-    $url = $url.'/index.php/http_api/send_sms?login='.$username.'&pass='.$password.'&to='.$mobile.'&message='.$msg;
-    
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);
+    $payload = json_encode([
+        'to' => [$mobile],
+        'text' => $msg
+    ]);
+
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'access-token: ' . $password
+    ]);
+    curl_setopt($ch, CURLOPT_FAILONERROR, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    
-    $ret = curl_exec($ch);    
+
+    $ret = curl_exec($ch);
     curl_close($ch);
-    
+
     if (!$ret) {
 	$error = "Invalid HTTP response";
 	return false;
